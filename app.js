cfcbffe90f79a1cfd3a6c4131565fb71d3cea3ee
Erin Witmer <ecwitmer@gmail.com>
2016-10-13 21:51:23 -0400

    final version 1

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-	
+
 
 });
 
 // single state variable
 var state = {
-	items: []
+    items: []
 };
 
 // state modification functions
@@ -14,17 +14,24 @@ var addItem = function(state, item) {
 };
 
 var deleteItem = function(state, item) {
-	var i = state.items.indexOf(item);
-		if(i != -1) {
-		state.items.splice(i, 1);
-		}
+
+
+var i = state.items.indexOf(item[0].firstChild.innerText);
+        if(i != -1) {
+        state.items.splice(i, 1);
+        }
+};
+
+var toggleCheck = function(item) {
+    item.toggleClass('shopping-item__checked');
 }
+
 
 // render functions
 var renderList = function(state, element) {
- 
+
 var itemsHTML = state.items.map(function(item){
-	return '<li><span class="shopping-item">'+item+'</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>'
+    return '<li><span class="shopping-item">'+item+'</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>'
 })
 
     element.html(itemsHTML);
@@ -33,16 +40,18 @@ var itemsHTML = state.items.map(function(item){
 // event listeners
 $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
-    addItem(state, $('#shopping-list-entry').val());  
+    addItem(state, $('#shopping-list-entry').val());
     renderList(state, $('.shopping-list'));
 });
 
+//on click of delete item
 $('.shopping-list').on('click', '.shopping-item-delete', function() {
     event.preventDefault();
-    deleteItem(state, //get current value//);  
+    deleteItem(state, $(this).closest('li'));
     renderList(state, $('.shopping-list'));
 });
 
+//on click of item toggle (check item off list)
 $('.shopping-list').on('click', '.shopping-item-toggle', function() {
-  	console.log('working1');
+    toggleCheck($(this.closest('li')));
 });
